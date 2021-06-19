636636a9e39a7c0466cefb25f08aea217d48081d
husein zolkepli <husein.zol05@gmail.com>
2021-06-19 16:02:23 +0800

    fix

--- apache-cluster/8.hive-impala/start.sh
+++ apache-cluster/8.hive-impala/start.sh
@@ -2,8 +2,6 @@
 
 /etc/init.d/ssh start
 
-$HADOOP_HOME/bin/hdfs namenode -format
-
 $HADOOP_HOME/sbin/start-yarn.sh
 $HADOOP_HOME/sbin/start-dfs.sh
 
@@ -11,9 +9,6 @@ $HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/hive/warehouse
 $HADOOP_HOME/bin/hdfs dfs -chmod 765 /user/hive/warehouse
 $HIVE_HOME/bin/schematool -initSchema -dbType derby
 
-$IMPALA_HOME/bin/impala-config.sh
-
-$IMPALA_HOME/bin/start-impala-cluster.py &
 $HIVE_HOME/bin/hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10000 --hiveconf hive.root.logger=INFO,console &
 wait
 
---
+++ apache-cluster/8.hive-impala/test.hql
@@ -0,0 +1,2 @@
+CREATE TABLE students (name VARCHAR(64), age INT, gpa DECIMAL(3,2));
+INSERT INTO TABLE students VALUES ('fred flintstone', 35, 1.28), ('barney rubble', 32, 2.32);
