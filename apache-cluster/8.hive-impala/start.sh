63fdded386248ccc067dd8253615d5cacf1fc290
husein zolkepli <husein.zol05@gmail.com>
2021-06-19 14:40:54 +0800

    fix start

--- apache-cluster/8.hive-impala/start.sh
+++ apache-cluster/8.hive-impala/start.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+/etc/init.d/ssh start
+
+$HADOOP_HOME/bin/hdfs namenode -format
+
+$HADOOP_HOME/sbin/start-yarn.sh
+$HADOOP_HOME/sbin/start-dfs.sh
+
+$HADOOP_HOME/bin/hdfs dfs -mkdir -p /user/hive/warehouse
+$HADOOP_HOME/bin/hdfs dfs -chmod 765 /user/hive/warehouse
+$HIVE_HOME/bin/schematool -initSchema -dbType derby
+
+$IMPALA_HOME/bin/impala-config.sh
+
 $IMPALA_HOME/bin/start-impala-cluster.py &
 $HIVE_HOME/bin/hive --service hiveserver2 --hiveconf hive.server2.thrift.port=10000 --hiveconf hive.root.logger=INFO,console &
 wait
